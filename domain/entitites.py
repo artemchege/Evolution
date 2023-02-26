5dec92d6977b391793eaf56f572428aecc41632e
artemchege <artemchege@me.com>
2023-02-26 11:01:51 +0200

    Matrix converted + new trained entity

--- domain/entitites.py
+++ domain/entitites.py
@@ -1,10 +1,25 @@
+import os
 from abc import ABC, abstractmethod
 import random
 from typing import List
 
+import numpy as np
+from stable_baselines3 import PPO
+
 from contrib.utils import logger
 
-from domain.objects import Movement, PrayFood
+from domain.objects import Movement, PrayFood, MOVEMENT_MAPPER_ADJACENT
+
+
+class MatrixConverter:
+
+    @staticmethod
+    def from_world_env_to_gym_env(matrix: List[List]) -> np:
+        replace_zeroes_to_ones = [[1 if x == 0 else x for x in row] for row in matrix]
+        replace_none_to_zeroes = [[0 if x is None or isinstance(x, HerbivoreNoBrain) else x for x in row] for row in
+                                  replace_zeroes_to_ones]
+        replace_food_to_two = [[2 if isinstance(x, PrayFood) else x for x in row] for row in replace_none_to_zeroes]
+        return np.array(replace_food_to_two).ravel()
 
 
 class AliveEntity(ABC):
@@ -26,7 +41,8 @@ class AliveEntity(ABC):
         self.lived_for += 1
 
 
-class PrayNoBrain(AliveEntity):
+class HerbivoreNoBrain(AliveEntity):
+    """ Not trained herbivore, movements are random """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -46,5 +62,19 @@ class PrayNoBrain(AliveEntity):
         return f'Pray {self.name}, health: {self.health}'
 
 
-class Predator:
-    pass
+class HerbivoreTrained100000(HerbivoreNoBrain):
+    """ Prev trained and saved model """
+
+    matrix_converted = MatrixConverter()
+    model_path = os.path.join('Training', 'saved_models', 'PPO_model_herbivore_100000_8x8_food50_5')
+    model = PPO.load(model_path)
+
+    def get_move(self, observation: List[List]) -> Movement:
+        self.health -= 1
+        self.increase_lived_for()
+
+        converted_observation = self.matrix_converted.from_world_env_to_gym_env(observation)
+        action_num, _ = self.model.predict(converted_observation)
+        movement: Movement = MOVEMENT_MAPPER_ADJACENT[int(action_num)]
+        logger.debug(f'{self} moves {movement} health {self.health}')
+        return movement
