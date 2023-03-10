bb16d1c974bb1bfbd2494baa52e39f47d787b3b6
artemchege <artemchege@me.com>
2023-03-10 08:25:48 +0200

    Main presafe

--- main.py
+++ main.py
@@ -1,14 +1,114 @@
-from domain.entitites import PrayNoBrain
-from domain.environment import Environment
+import os
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+from contrib.utils import logger
+from domain.entitites import HerbivoreNoBrain, HerbivoreTrained100000
+from domain.environment import EnvironmentTrainRegime, EnvironmentLiveRegime
+from domain.objects import Movement, Coordinates, MOVEMENT_MAPPER_ADJACENT, TrainingSetup
+from evolution.training import HerbivoreTrainingRunner
 from visualization.visualize import Visualizer
 
 
-def main():
-    environment = Environment(15, 15)
-    pray_1 = PrayNoBrain('Mammoth', 10)
-    pray_2 = PrayNoBrain('Dodo bird', 15)
-    environment.setup_initial_state(live_objs=[pray_1, pray_2], pray_foods=50, nutrition=5)
+def visualize_trained_model_visualizer_runner():
+    # Визуализация тренированной модели через мое окружение
+    environment = EnvironmentLiveRegime(15, 15, True)
+    # pray_1 = HerbivoreNoBrain('Mammoth', 10)
+    # pray_2 = HerbivoreNoBrain('Dodo bird', 15)
+    trained_prey = HerbivoreTrained100000('Crow', 10)
+    environment.setup_initial_state(live_objs=[trained_prey], herbivore_food_amount=25, nutrition=5)
     Visualizer(environment).run()
+
+
+def visualize_training():
+    # Визуализация тренировки
+    setup: TrainingSetup = TrainingSetup(
+        herbivore_food_amount=50,
+        herbivore_food_nutrition=2,
+        replenish_food=True,
+        living_object_name='Mammoth',
+        living_object_class=HerbivoreNoBrain,
+        living_object_initial_health=10,
+        live_length=5000,
+    )
+    episodes = 5000
+    train_env_v = HerbivoreTrainingRunner(
+        movement_class=Movement,
+        environment=EnvironmentTrainRegime(
+            width=16,
+            height=16,
+            replenish_food=setup.replenish_food,
+            food_nutrition=setup.herbivore_food_nutrition,
+        ),
+        setup=setup,
+    )
+    model = PPO("MlpPolicy", train_env_v, verbose=1, tensorboard_log=None)
+    for i in range(episodes):
+        print(f'Episode {i} started')
+        observ = train_env_v.reset()
+        done = False
+        score = 0
+
+        # Подменяем окружения и обучаем модель, как закончили, меняем окружение обратно и визуализируем, получается
+        # так что на отдельном окружении тренируем, на отдельном показываем, как только в режиме показа сущность
+        # умерла - отправляем на новую тренировку. _v - визуализация, _l - обучение.
+        train_env_l = HerbivoreTrainingRunner(
+            movement_class=Movement,
+            environment=EnvironmentTrainRegime(
+                width=16,
+                height=16,
+                replenish_food=True,
+                food_nutrition=setup.herbivore_food_nutrition,
+            ),
+            setup=setup,
+        )
+        model.set_env(train_env_l)
+        model.learn(total_timesteps=1)
+        model.set_env(train_env_v)
+
+        while not done:
+            action, _ = model.predict(observ)
+            observ, reward, done, info = train_env_v.step(int(action))
+            score += reward
+            train_env_v.render()
+
+        print('Episode {} Finished. Score:{}'.format(i, score))
+        train_env_v.close()
+
+
+def visualize_trained_model_evaluate_policy():
+    # Визуализация оттренированной модели через evaluate_policy
+    save_path = os.path.join('Training', 'saved_models', 'PPO_model_Pray_100000')
+    model = PPO.load(save_path)
+    # environment = Environment(16, 16)
+    # train_env = HerbivoreTrainingRunner(Movement, environment)
+    # evaluate_policy(model, train_env, n_eval_episodes=10, render=True)
+
+
+def benchmark_herbivore_no_brain():
+    # Бенчмарк травоядного без мозга
+    results_lived_for = []
+    for episode in range(500):
+        pass
+
+        # setup
+        # environment = Environment(15, 15)
+        # pray = PrayNoBrain('Mammoth', 10)
+        # environment.setup_initial_state(live_objs=[pray], pray_foods=50, nutrition=5)
+        #
+        # # run
+        # game_over = False
+        # while not game_over:
+        #     _, game_over = environment.step_living_regime()
+        # results_lived_for.append(pray.lived_for)
+
+    logger.info(f'Average lived for is: {sum(results_lived_for)/len(results_lived_for)}')
+
+
+def main():
+    visualize_trained_model_visualizer_runner()
 
 
 if __name__ == '__main__':
